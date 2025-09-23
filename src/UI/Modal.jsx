f9fe2e540743cff1c952406be0ca76e06b4fc748
Davitamane <datokhizanishvili005@gmail.com>
2025-09-23 19:54:34 +0400

    Fully finished static UI

--- src/UI/Modal.jsx
+++ src/UI/Modal.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import close from "../assets/Close.svg";
+import success from "../assets/Success.svg";
+import Button from "./Button";
+import { Link } from "react-router-dom";
 
 function Modal({ isModalOpen, setIsModalOpen }) {
   const modalRef = useRef(null);
@@ -24,15 +27,35 @@ function Modal({ isModalOpen, setIsModalOpen }) {
   return createPortal(
     <div className="fixed top-0 left-0 w-full h-screen bg-black/20 flex items-center justify-center z-50 backdrop-blur-[3px]">
       <div
-        className="flex w-219  justify-center flex-col gap-6 bg-white p-10 rounded-lg shadow-lg"
+        className="flex w-219 h-147.5 flex-col gap-11 bg-white p-10 rounded-lg shadow-lg"
         // h-147.5
         ref={modalRef}
         id="modalForm"
       >
         <div className="flex justify-end">
-          <button>
-            <img src={close} />
-          </button>
+          <Link to="/products">
+            <button onClick={() => setIsModalOpen(false)}>
+              <img src={close} />
+            </button>
+          </Link>
+        </div>
+        <div className="flex justify-center h-full">
+          <div className="flex flex-col items-center gap-10 w-fit">
+            <div>
+              <img src={success} alt="" />
+            </div>
+            <div className="flex flex-col gap-4 items-center">
+              <h1 className="font-bold text-5xl">Congrats!</h1>
+              <p>Your order is placed successfully!</p>
+            </div>
+            <div className="w-full mt-8.5">
+              <Link to="/products">
+                <Button onClick={() => setIsModalOpen(false)}>
+                  Continue shopping
+                </Button>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>,
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,7 +9,7 @@ function Login() {
   return (
     <div className="grid grid-cols-2 min-h-100">
       <div>
-        <img src={image} className="h-screen" />
+        <img src={image} className="w-237 h-248" />
       </div>
       <div className="flex mx-40 items-center">
         {login ? (
